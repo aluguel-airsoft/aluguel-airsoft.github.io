bf4bbe1e04e37d2b37b41088375f1ec152bcb9e9
Mauricio Michels <mauricio@kugel.com.br>
2017-11-28 21:46:27 -0200

    db feed

--- js/db.js
+++ js/db.js
@@ -6,11 +6,18 @@ var precoBalaclava	= 2.0;
 var precoColete		= 5.0;
 var precoColdre		= 4.0;
 
+//								0		1		2			3		   4		  5			6
 var equipamentosExistentes = ["M4", "Glock", "Óculos", "Máscara", "Balaclava", "Colete", "Coldre"];
 
 var reservas = [
 	{
 		"data": "17/12/2017",
+		"locador": "",
 		"equipamentos":[equipamentosExistentes[0], equipamentosExistentes[2], equipamentosExistentes[4]]
+	},
+	{
+		"data": "03/12/2017",
+		"locador": "Gean Back Viper",
+		"equipamentos":[equipamentosExistentes[3]]
 	}
 ];
