33d6d08cafc7212f83ff2a24a4ea0eba76242a4c
Maurício Michels <mauricio@kugel.com.br>
2017-11-28 11:34:23 -0200

    incluido foto viper

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-document.getElementById("ano").textContent = new Date().getFullYear();
+$( document ).ready(function() {
+
+	document.getElementById("ano").textContent = new Date().getFullYear();
+
+	$( "#confirmar" ).click(function() {
+		alert( "Handler for .click() called." );
+	});
+
+});
